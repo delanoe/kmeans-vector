7d65c295b30f489888cc6c83bfb048d855687f06
Alexandre Delanoë <anoe@debian.org>
2016-07-20 07:56:21 +0200

    [IMPORT RENAME] to keep compatibility with tests.

--- Math/KMeans.hs
+++ Math/KMeans.hs
@@ -38,8 +38,8 @@ module Math.KMeans
   ) where
 
 import Control.Monad.Identity
-import qualified Data.Vector.Unboxed as U
-import qualified Data.Vector as V
+import qualified Data.Vector.Unboxed as V
+import qualified Data.Vector as G
 import qualified Data.List as L
 import Data.Function (on)
 
@@ -50,34 +50,34 @@ import Data.Traversable
 #endif
 
 -- | A distance on vectors
-type Distance = U.Vector Double -> U.Vector Double -> Double
+type Distance = V.Vector Double -> V.Vector Double -> Double
 
 -- | The euclidean distance without taking the final square root
 --   This would waste cycles without changing the behavior of the algorithm
 euclidSq :: Distance
-euclidSq v1 v2 = U.sum $ U.zipWith diffsq v1 v2
+euclidSq v1 v2 = V.sum $ V.zipWith diffsq v1 v2
   where diffsq a b = (a-b)^(2::Int)
 {-# INLINE euclidSq #-}
 
 -- | L1 distance of two vectors: d(v1, v2) = sum on i of |v1_i - v2_i|
 l1dist :: Distance
-l1dist v1 v2 = U.sum $ U.zipWith diffabs v1 v2
+l1dist v1 v2 = V.sum $ V.zipWith diffabs v1 v2
   where diffabs a b = abs (a - b)
 {-# INLINE l1dist #-}
 
 -- | L-inf distance of two vectors: d(v1, v2) = max |v1_i - v2_i]
 linfdist :: Distance
-linfdist v1 v2 = U.maximum $ U.zipWith diffabs v1 v2
+linfdist v1 v2 = V.maximum $ V.zipWith diffabs v1 v2
   where diffabs a b = abs (a - b)
 {-# INLINE linfdist #-}
 
--- | This is what 'kmeans' hands you back. It's just a 'V.Vector' of clusters
+-- | This is what 'kmeans' hands you back. It's just a 'G.Vector' of clusters
 --   that will hopefully be of length 'k'.
-type Clusters a = V.Vector (Cluster a)
+type Clusters a = G.Vector (Cluster a)
 
 -- | This type is used internally by 'kmeans'. It represents our (hopefully)
 --   @k@ centroids, obtained by computing the new centroids of a 'Cluster'
-type Centroids  = V.Vector (U.Vector Double)
+type Centroids  = G.Vector (V.Vector Double)
 
 -- | A 'Cluster' of points is just a list of points
 newtype Cluster a =
@@ -90,14 +90,14 @@ clusterAdd (Cluster c) x = Cluster (x:c)
 emptyCluster :: Cluster a
 emptyCluster = Cluster []
 
-addCentroids :: U.Vector Double -> U.Vector Double -> U.Vector Double
-addCentroids v1 v2 = U.zipWith (+) v1 v2
+addCentroids :: V.Vector Double -> V.Vector Double -> V.Vector Double
+addCentroids v1 v2 = V.zipWith (+) v1 v2
 
 -- | This is the current partitionning strategy used
 --   by 'kmeans'. If we want @k@ clusters, we just 
 --   try to regroup consecutive elements in @k@ buckets
 partition :: Int -> [a] -> Clusters a
-partition k vs = V.fromList $ go vs
+partition k vs = G.fromList $ go vs
   where go l = case L.splitAt n l of
           (vs', []) -> [Cluster vs']
           (vs', vss) -> Cluster vs' : go vss
@@ -111,10 +111,10 @@ partition k vs = V.fromList $ go vs
 -- using 'distance' to measure the distance between vectors,
 -- trying to separate 'points' in 'k' clusters.
 --
--- Extracting features just means getting a 'U.Vector'
+-- Extracting features just means getting a 'V.Vector'
 -- with 'Double' coordinates that will represent your type
 -- in the space in which 'kmeans' will run.
-kmeans :: (a -> U.Vector Double) -- ^ feature extraction
+kmeans :: (a -> V.Vector Double) -- ^ feature extraction
        -> Distance               -- ^ distance function
        -> Int                    -- ^ the 'k' to run 'k'-means with (i.e number of desired clusters)
        -> [a]                    -- ^ input list of 'points'
@@ -123,7 +123,7 @@ kmeans extract dist k points =
   runIdentity $ kmeansWith (\n ps -> pure $ partition n ps) extract dist k points
 
 
-kmeans' :: (a -> U.Vector Double) -- ^ feature extraction
+kmeans' :: (a -> V.Vector Double) -- ^ feature extraction
        -> Distance               -- ^ distance function
        -> Int                    -- ^ the 'k' to run 'k'-means with (i.e number of desired clusters)
        -> [a]                    -- ^ input list of 'points'
@@ -133,7 +133,7 @@ kmeans' extract dist k points =
 
 
 -- | Same as 'kmeans', except that instead of using 'partition', you supply your own
---   function for choosing the initial clusterinV. Two important things to note:
+--   function for choosing the initial clusterinG. Two important things to note:
 -- 
 --   * If you don't need any kind of effect and just have a 'partition'-like function
 --     you want to use, @m@ will can just be 'Identity' here. If that's too 
@@ -143,13 +143,13 @@ kmeans' extract dist k points =
 -- 
 --   * Most likely, you want to have something smarter than our simple 'partition' function.
 --     A couple of papers I have read claim very decent results by using some precise
---     probabilistic schemas for the initial partitionninV. In this case, your @m@ would
+--     probabilistic schemas for the initial partitionninG. In this case, your @m@ would
 --     probably be 'IO' or 'ST' (e.g using my <http://hackage.haskell.org/package/probable probable> package)
 --     and you could fine-tune the way the initial clusters are picked so that the algorithm
 --     may give better results. Of course, if your initialization is monadic, so is the result. 
 kmeansWith :: forall m a. Monad m
            => (Int -> [a] -> m (Clusters a)) -- ^ how should we partition the points?
-           -> (a -> U.Vector Double)         -- ^ get the coordinates of a "point"
+           -> (a -> V.Vector Double)         -- ^ get the coordinates of a "point"
            -> Distance                       -- ^ what distance do we use
            -> Int                            -- ^ number of desired clusters
            -> [a]                            -- ^ list of points
@@ -167,17 +167,17 @@ kmeansWith initF extract dist k points = go `liftM` initF k points
     kmeansStep clusters = 
       case centroidsOf clusters of
         centroids -> 
-            V.filter (not . null . elements)
-          . V.unsafeAccum clusterAdd (V.replicate k emptyCluster)
+            G.filter (not . null . elements)
+          . G.unsafeAccum clusterAdd (G.replicate k emptyCluster)
           . map (pairToClosestCentroid centroids)
           $ points
 
     centroidsOf :: Clusters a -> Centroids
-    centroidsOf cs = V.map centroidOf cs
+    centroidsOf cs = G.map centroidOf cs
       where
 
         centroidOf (Cluster elts) = 
-            U.map (/n) 
+            V.map (/n) 
           . L.foldl1' addCentroids
           $ map extract elts
 
@@ -185,15 +185,15 @@ kmeansWith initF extract dist k points = go `liftM` initF k points
 
     pairToClosestCentroid :: Centroids -> a -> (Int, a)
     pairToClosestCentroid cs a = (minDistIndex, a)
-      where !minDistIndex = V.minIndexBy (compare `on` dist (extract a)) cs
+      where !minDistIndex = G.minIndexBy (compare `on` dist (extract a)) cs
 
     pgroupsEqualUnder :: Clusters a -> Clusters a -> Bool
     pgroupsEqualUnder g1 g2 = elementsOf g1 == elementsOf g2
-      where elementsOf = V.map (map extract . elements)
+      where elementsOf = G.map (map extract . elements)
 
 kmeansWith' :: forall m a. Monad m
            => (Int -> [a] -> m (Clusters a)) -- ^ how should we partition the points?
-           -> (a -> U.Vector Double)         -- ^ get the coordinates of a "point"
+           -> (a -> V.Vector Double)         -- ^ get the coordinates of a "point"
            -> Distance                       -- ^ what distance do we use
            -> Int                            -- ^ number of desired clusters
            -> [a]                            -- ^ list of points
@@ -211,17 +211,17 @@ kmeansWith' initF extract dist k points = go `liftM` initF k points
     kmeansStep clusters =
       case centroidsOf clusters of
         centroids -> (centroids, clusters) where
-            clusters = V.filter (not . null . elements)
-                       . V.unsafeAccum clusterAdd (V.replicate k emptyCluster)
+            clusters = G.filter (not . null . elements)
+                       . G.unsafeAccum clusterAdd (G.replicate k emptyCluster)
                        . map (pairToClosestCentroid centroids)
                        $ points
 
     centroidsOf :: Clusters a -> Centroids
-    centroidsOf cs = V.map centroidOf cs
+    centroidsOf cs = G.map centroidOf cs
       where
 
         centroidOf (Cluster elts) =
-            U.map (/n) 
+            V.map (/n) 
           . L.foldl1' addCentroids
           $ map extract elts
 
@@ -229,11 +229,11 @@ kmeansWith' initF extract dist k points = go `liftM` initF k points
 
     pairToClosestCentroid :: Centroids -> a -> (Int, a)
     pairToClosestCentroid cs a = (minDistIndex, a)
-      where !minDistIndex = V.minIndexBy (compare `on` dist (extract a)) cs
+      where !minDistIndex = G.minIndexBy (compare `on` dist (extract a)) cs
 
     pgroupsEqualUnder :: Clusters a -> Clusters a -> Bool
     pgroupsEqualUnder g1 g2 = elementsOf g1 == elementsOf g2
-      where elementsOf = V.map (map extract . elements)
+      where elementsOf = G.map (map extract . elements)
 
 
 {-# INLINE kmeansWith #-}
